13669628e3e0adb926e10ada67ff73ff4e1d28ba
TimTechDev <github@timtechdev.de>
2023-12-04 14:32:43 +0100

    feat(2023): Solution for day 04
    Signed-off-by: TimTechDev <github@timtechdev.de>

---
+++ year2023/src/day04.rs
@@ -0,0 +1,108 @@
+#[derive(Debug)]
+struct Card {
+    winning: Vec<usize>,
+    drawn: Vec<usize>,
+}
+
+fn parse_line(input: &str) -> Card {
+    let (w, d) = input.split_once(':').unwrap().1.split_once('|').unwrap();
+
+    return Card {
+        winning: w
+            .trim()
+            .split_ascii_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect(),
+        drawn: d
+            .trim()
+            .split_ascii_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect(),
+    };
+}
+
+#[aoc_generator(day4)]
+fn parse(input: &str) -> Vec<Card> {
+    return input.lines().map(parse_line).collect();
+}
+
+fn part1_score(count: usize) -> usize {
+    if count == 0 {
+        return 0;
+    } else {
+        return 1 << (count - 1);
+    }
+}
+
+#[aoc(day4, part1)]
+fn part1(cards: &[Card]) -> usize {
+    return cards
+        .iter()
+        .map(|card| {
+            part1_score(
+                card.drawn
+                    .iter()
+                    .filter(|d| card.winning.contains(*d))
+                    .count(),
+            )
+        })
+        .sum();
+}
+
+#[aoc(day4, part2)]
+fn part2(cards: &[Card]) -> usize {
+    let mut amounts: Vec<usize> = cards.iter().map(|_| 1_usize).collect();
+    let mut position: usize = 0;
+
+    for card in cards {
+        let count = card
+            .drawn
+            .iter()
+            .filter(|d| card.winning.contains(*d))
+            .count();
+        let amount = amounts.get(position).unwrap_or(&0).clone();
+
+        // println!("card: {} amount: {} count: {}", position + 1, amount, count);
+
+        if count == 0 {
+            position += 1;
+            continue;
+        }
+
+        for pos in (position + 1)..=(position + count) {
+            amounts[pos] = amounts[pos] + amount;
+        }
+        // println!("amounts: {:?}", amounts);
+
+        position += 1;
+    }
+
+    return amounts.iter().sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_1: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    #[test]
+    fn test_parser() {
+        let result = parse(EXAMPLE_1);
+        println!("{:?}", result);
+        for card in result {
+            assert_eq!(5, card.winning.len());
+            assert_eq!(8, card.drawn.len());
+        }
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(13, part1(&parse(EXAMPLE_1)));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(30, part2(&parse(EXAMPLE_1)));
+    }
+}
